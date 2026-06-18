c7e4e9f95a36e5a2bf4da524cbdddac82428a838
aufam <aufa@comtrue.com>
2026-06-18 20:13:30 +0700

    apply changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <filesystem>
+#include <reproc++/run.hpp>
 
 import carton;
 
@@ -13,61 +14,94 @@ namespace fs = std::filesystem;
 constexpr auto toml_version = cpx::toruniina_toml::spec::v(1, 1, 0);
 
 int main(int argc, char **argv) {
+    // spdlog
     auto sink = std::make_shared<spdlog::sinks::stderr_color_sink_mt>();
     spdlog::set_default_logger(std::make_shared<spdlog::logger>("carton", std::move(sink)));
     spdlog::set_pattern("%^%l%$: %v");
 
+    // cli
     Cli cli   = {};
     cli.cache = std::getenv("HOME") + std::string("/.carton");
 
     cpx::cli11::parse("C++ package manager", argc, argv, cli);
     spdlog::set_level(cli.log_level);
 
+    // context
     Cache  cache = {};
     Carton ctx   = {};
 
     ctx.cli   = &cli;
     ctx.cache = &cache;
 
-    fs::path registry = "registry.toml";
-    if (!fs::exists(registry))
-        registry = fs::path(cli.cache) / registry;
+    // parse registry
+    const auto registry_path = [&]() {
+        fs::path ret = ".carton/registry.toml";
+        if (!fs::exists(ret))
+            ret = fs::path(cli.cache) / "registry.toml";
+        return ret;
+    }();
 
     try {
-        cpx::toruniina_toml::parse_from_file(registry.string(), ctx.registry, toml_version);
+        cpx::toruniina_toml::parse_from_file(registry_path.string(), ctx.registry, toml_version);
     } catch (std::exception &e) {
         spdlog::error("Failed to parse carton packages: {}", e.what());
         return 1;
     }
 
+    // parse config
+    const auto config_path = [&]() {
+        fs::path ret = ".carton/config.toml";
+        if (!fs::exists(ret))
+            ret = fs::path(cli.cache) / "config.toml";
+        return ret;
+    }();
+
+    if (fs::exists(config_path))
+        try {
+            auto cfg     = cpx::toruniina_toml::parse_from_file<Config>(config_path.string(), toml_version);
+            ctx.profiles = cfg.profiles;
+        } catch (std::exception &e) {
+            spdlog::error("Failed to parse carton config: {}", e.what());
+            return 1;
+        }
+
+    // parse context
     try {
         cpx::toruniina_toml::parse_from_file("./carton.toml", ctx, toml_version);
-        if (fs::path(ctx.lib.path).is_absolute() && fs::path(ctx.lib.subdir).is_absolute())
-            throw ferr("Path must be relative");
     } catch (std::exception &e) {
         spdlog::error("Failed to parse carton package: {}", e.what());
         return 1;
     }
-    ctx.lib.path = (fs::current_path() / ctx.lib.path).string();
 
-    ctx.profiles.dev._module_support =
-        ctx.profiles.dev.modules == "auto" && ctx.profiles.dev.cxx.find("clang++") != std::string::npos;
-    ctx.profiles.release._module_support =
-        ctx.profiles.release.modules == "auto" && ctx.profiles.release.cxx.find("clang++") != std::string::npos;
-    spdlog::info("provile.dev._module_support={}", ctx.profiles.dev._module_support);
-    spdlog::info("provile.release._module_support={}", ctx.profiles.release._module_support);
+    // validate
+    if (fs::path(ctx.lib.path).is_absolute() || fs::path(ctx.lib.subdir).is_absolute()) {
+        spdlog::error("<carton>.lib.path must be relative");
+        return 1;
+    }
+    ctx.lib.path = (fs::current_path() / ctx.lib.path).string();
+    ctx.profiles.check_module_support();
+
+    // execute
+    const bool  manifest = cli.manifest.has_value();
+    const bool  run      = cli.run.has_value();
+    const bool  build    = cli.build.has_value();
+    const bool  release  = run ? cli.run->release : build ? cli.build->release : manifest ? cli.manifest->release : cli.release;
+    const auto &profile  = release ? ctx.profiles.release : ctx.profiles.dev;
+    const auto &features = run        ? cli.run->features
+                           : build    ? cli.build->features
+                           : manifest ? cli.manifest->features
+                                      : cli.features;
+
+    ctx.no_default_features = run        ? cli.run->no_default_features
+                              : build    ? cli.build->no_default_features
+                              : manifest ? cli.manifest->no_default_features
+                                         : cli.no_default_features;
 
     std::vector<CompileCommand> ccs;
-
-    const bool  do_run       = cli.run.has_value();
-    const bool  do_build     = do_run || cli.build.has_value();
-    const bool  release_mode = do_run ? cli.run->release : do_build ? cli.build->release : false;
-    const auto &profile      = release_mode ? ctx.profiles.release : ctx.profiles.dev;
-
     try {
-        ctx.configure(profile);
-        auto [_, m] = ctx.build(profile, ccs, do_build);
-        if (do_run)
+        ctx.configure(profile, features);
+        auto [_, m] = ctx.build(profile, ccs, build || run);
+        if (run)
             return ctx.run(m);
     } catch (std::exception &e) {
         auto of = std::ofstream("./compile_commands.json");
@@ -79,7 +113,7 @@ int main(int argc, char **argv) {
     auto of = std::ofstream("./compile_commands.json");
     of << cpx::yy_json::dump(ccs, YYJSON_WRITE_PRETTY_TWO_SPACES);
 
-    if (cli.manifest.has_value())
+    if (manifest)
         fmt::println("{}", cpx::yy_json::dump(ctx));
 
     return 0;
